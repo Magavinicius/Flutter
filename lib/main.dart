3a1e6a34089bc89672080896f97a1890a9929225
Vinícius Magalhães Batista <63367957+Magavinicius@users.noreply.github.com>
2022-07-03 16:58:31 -0300

    Add botão

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(new PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('Pergunta renpondida');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua aventura favorita?',
+      'Qual é a sua raça preferida?',
+    ];
+
     return MaterialApp(
-      home: Text('Olá flutter!!!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Adventurer\'s Sheets'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
