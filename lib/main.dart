ed211abdc67e5106784f77ff8b8427f8166dd20a
Vinícius Magalhães Batista <63367957+Magavinicius@users.noreply.github.com>
2022-07-03 15:43:14 -0300

    Olá flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+main() {
+  runApp(new PerguntaApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
+class PerguntaApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      home: Text('Olá flutter!!!'),
     );
   }
 }
